d87ee49e9d48eb20d58ee860b1a66f4d962ca6e9
Владислав Лисянский <vladislav.lisyanskiy@gmail.com>
2016-10-17 20:08:22 +0700

    Initial Commit

---
+++ u2fFramework/u2fFramework.h
@@ -0,0 +1,19 @@
+//
+//  u2fFramework.h
+//  u2fFramework
+//
+//  Created by Владислав Лисянский on 17.10.16.
+//  Copyright © 2016 Владислав Лисянский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for u2fFramework.
+FOUNDATION_EXPORT double u2fFrameworkVersionNumber;
+
+//! Project version string for u2fFramework.
+FOUNDATION_EXPORT const unsigned char u2fFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <u2fFramework/PublicHeader.h>
+
+
